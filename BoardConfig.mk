9dfcc001f68f01600f55eb54aee5e32590c58441
Arian <arian.kulmer@web.de>
2026-01-22 19:18:48 +0100

    mumba: Drop QTI power hal and migrate to libperfmgr
    Change-Id: I1f0a5fcf281f48e9610e521ec0ef07d7e1ae6dc0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,6 +182,7 @@ ODM_MANIFEST_NE_FILES := $(DEVICE_PATH)/configs/vintf/sku/manifest_ne.xml
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # SEPolicy
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/motorola/sepolicy/qti/SEPolicy.mk
 
